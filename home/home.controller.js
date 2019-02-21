38acd253622ef2e620726c11107c4606aec180a5
Arun Kumar Hosalli <cemows@gmail.com>
2019-02-21 21:42:22 +0530

    angular app with login and list user

---
+++ home/home.controller.js
@@ -0,0 +1,64 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('HomeController', HomeController);
+
+    HomeController.$inject = ['$scope', 'GitService', 'UserService', '$rootScope', '$http'];
+
+    function HomeController($scope, GitService, UserService, $rootScope, $http) {
+        var vm = this;
+
+        vm.user = null;
+        vm.allUsers = [];
+        vm.deleteUser = deleteUser;
+        vm.sortOrder = '';
+        $scope.searchText = '';
+        initController();
+
+        function initController() {
+            loadCurrentUser();
+            loadAllUsers();
+            // loadGitUser();
+            takeGitUser();
+        }
+
+
+        $scope.sortIt = function () {
+            vm.sortOrder == 'login' ? vm.sortOrder = 'id' : vm.sortOrder = 'login';
+        }
+
+        function loadCurrentUser() {
+            UserService.GetByUsername($rootScope.globals.currentUser.username)
+                .then(function (user) {
+                    vm.user = user;
+                });
+        }
+
+        function takeGitUser() {
+            debugger;
+            vm.gitUser = GitService.GetAll();
+
+        }
+
+
+
+
+
+        function loadAllUsers() {
+            UserService.GetAll()
+                .then(function (users) {
+                    vm.allUsers = users;
+                });
+        }
+
+        function deleteUser(id) {
+            UserService.Delete(id)
+                .then(function () {
+                    loadAllUsers();
+                });
+        }
+    }
+
+})();
